daf16164f005c58e6fce87920ef91b9a78d61009
Justin Murphy <justin.murphy@zyolith.com>
2017-01-02 18:21:15 -0600

    Update imports for new file strcture

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './polyfills.ts';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
+import { environment } from './app/environments/environment';
 import { AppModule } from './app/';
 
 if (environment.production) {
