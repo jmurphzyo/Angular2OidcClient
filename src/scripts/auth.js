67f1777d460675f9cdcd88af60083f404508eb4f
Colin Rudd <colin@dynamo.ai>
2017-08-17 05:00:09 -0400

    fixed bug; prefix UserManager call with Oidc call

--- src/scripts/auth.js
+++ src/scripts/auth.js
@@ -2,7 +2,7 @@
 if ((Oidc && Oidc.Log && Oidc.Log.logger)) {
     Oidc.Log.logger = console;
 }
-new UserManager().signinRedirectCallback().then(function (user) {
+new Oidc.UserManager().signinRedirectCallback().then(function (user) {
     if (user == null) {
         document.getElementById("waiting").style.display = "none";
         document.getElementById("error").innerText = "No sign-in request pending.";
